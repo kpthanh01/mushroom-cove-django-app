202aae36149759be46bb53fcbc67000bf1b8e63f
kpthanh01 <kphamthanh01@gmail.com>
2024-12-09 14:37:49 -0500

    Added User authentication features; revise the mushroom model to include users; added and revise html files

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,9 +10,11 @@ class Mushroom(models.Model):
   scientificName = models.CharField(max_length=100)
   description = models.TextField(max_length=500)
 
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
   def __str__(self):
     return self.commonName
   
   def get_absolute_url(self):
-      return reverse("mushroom_detail", kwargs={"mushroom_id": self.pk})
+      return reverse("mushroom-detail", kwargs={"mushroom_id": self.pk})
   
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-  return HttpResponse('<h1>Mushroom Cove</h1>')
+# Classes
+class Home(LoginView):
+  template_name = 'home.html'
+
+# Functions
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid Sign Up - Try Again'
+  form = UserCreationForm()
+  context = {
+    'form': form,
+    'error_message': error_message
+  }
+  return render(request, 'signup.html', context)
